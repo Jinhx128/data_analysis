4149933c897172062fe8e779088e6524f2dd8641
luoyusoft <luoyusoft@126.com>
2020-11-27 16:41:19 +0800

    1. 新增脚本

--- script/brush_data.py
+++ script/brush_data.py
@@ -14,13 +14,13 @@ def brush_mysql():
     print('到点了，开始刷数据！')
     for item in tables:
         # 修改数据的SQL语句
-        sql = 'update {} set like_num = like_num + {} , read_num = read_num + {}'.format(item, count, count)
+        sql = 'update {} set read_num = read_num + {} where publish = 1'.format(item, count, count)
         try:
             # 执行SQL语句
             execute = cursor.execute(sql)
             # 提交事务
             conn.commit()
-            print('{}-{}表更新了{}条数据，每条数据阅读量，点赞量各加{}！'.format(
+            print('{}-{}表更新了{}条数据，每条数据阅读量加{}！'.format(
                 datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S'), item, execute, count))
 
         except Exception as e:
--- script/study.sh
+++ script/study.sh
@@ -1,1 +1,1 @@
-nohup python - u brush_data.py > brush_data.log 2>&1 &
+nohup python3 -u brush_data.py > brush_data.log 2>&1 &
